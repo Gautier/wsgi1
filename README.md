02368c62f42bdad35aeaaf027975f7fce6cffd5d
Gautier Hayoun <ghayoun@gmail.com>
2011-04-16 15:04:14 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# python wsgi application cloudfoundry compatible
+
+This is a sample python wsgi application that runs on cloudfoundry.
+
+Demonstrate Python support of https://github.com/gautier/vcap/ and https://github.com/gautier/vmc/.
---
+++ web.py
@@ -0,0 +1,51 @@
+from cgi import escape
+from wsgiref.simple_server import make_server
+import os
+
+ZEN = """Gur Mra bs Clguba, ol Gvz Crgref
+
+Ornhgvshy vf orggre guna htyl.
+Rkcyvpvg vf orggre guna vzcyvpvg.
+Fvzcyr vf orggre guna pbzcyrk.
+Pbzcyrk vf orggre guna pbzcyvpngrq.
+Syng vf orggre guna arfgrq.
+Fcnefr vf orggre guna qrafr.
+Ernqnovyvgl pbhagf.
+Fcrpvny pnfrf nera'g fcrpvny rabhtu gb oernx gur ehyrf.
+Nygubhtu cenpgvpnyvgl orngf chevgl.
+Reebef fubhyq arire cnff fvyragyl.
+Hayrff rkcyvpvgyl fvyraprq.
+Va gur snpr bs nzovthvgl, ershfr gur grzcgngvba gb thrff.
+Gurer fubhyq or bar-- naq cersrenoyl bayl bar --boivbhf jnl gb qb vg.
+Nygubhtu gung jnl znl abg or boivbhf ng svefg hayrff lbh'er Qhgpu.
+Abj vf orggre guna arire.
+Nygubhtu arire vf bsgra orggre guna *evtug* abj.
+Vs gur vzcyrzragngvba vf uneq gb rkcynva, vg'f n onq vqrn.
+Vs gur vzcyrzragngvba vf rnfl gb rkcynva, vg znl or n tbbq vqrn.
+Anzrfcnprf ner bar ubaxvat terng vqrn -- yrg'f qb zber bs gubfr!"""
+
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+
+    yield '<html><head><title>Python in the clouds!</title></head><body>'
+    yield '<h1>FastCGI Environment</h1>'
+    yield '<table>'
+    for k, v in sorted(environ.items()):
+         yield '<tr><th>%s</th><td>%s</td></tr>' % (k, v)
+    yield '</table>'
+
+    d = {}
+    for c in (65, 97):
+        for i in range(26):
+            d[chr(i+c)] = chr((i+13) % 26 + c)
+
+    yield '<h1>I am *really* PYTHON!</h1>'
+    yield "".join([d.get(c, c) for c in ZEN]).replace("\n", "<br/>")
+
+    yield '</body></html>'
+
+PORT = int(os.environ["VCAP_APP_PORT"])
+httpd = make_server('', PORT, app)
+print "Serving HTTP on port %d..." % PORT
+
+httpd.serve_forever()
